9f3ae866336e6cf54e7eb6d1152bab5adef1def3
Alexandru Enache <alexandru.enache5@ing.com>
2022-11-24 13:13:08 +0200

    es6-10 features

---
+++ src/es6-10-features.js
@@ -0,0 +1,50 @@
+
+/* ----------------------- */
+/*  ES 7,8,9,10 features:  */
+/* ----------------------- */
+
+/* 1. ES 7&8 Features: */
+
+// You can initialize the class properties outside of constructor
+// string.padEnd() and string.padStart() functions
+// Exponential Operator
+// Trailing comma
+// Object.values() and Object.entries()
+// array.prototype.includes() 
+// Async Functions   - ES8
+// object.getOwnPropertyDescriptors()
+
+
+// 2. ES 9 Features:
+
+// RegEx changes 
+// Rest/Spread properties
+// Asynchronous iteration
+    // async function* foo() {
+    //     yield 1;
+    //     yield 2;
+    // }
+    
+    // (async function() {
+    //     for await (const num of foo()) {
+    //     console.log(num);
+    //     // expected output: 1
+    
+    //     break; // closes iterator, triggers return
+    //     }
+    // })();
+
+
+// 3. ES10 Features
+
+// Array.flat()
+// Array.flatmap()
+// object.fromentries()
+// String.trimstart() and String.trimend()
+// Optional catch binding
+// toString() - revised
+// sym.description
+
+
+// https://www.cronj.com/blog/javascript-es7-es8-new-features/
+// https://madasamy.medium.com/javascript-brief-history-and-ecmascript-es6-es7-es8-features-673973394df4
